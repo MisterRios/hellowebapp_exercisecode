bc66d98d1d0de825b275c6cf64fc48a7d14071aa
misterrios <misterrios@gmail.com>
2015-01-09 23:28:26 +0100

    finished chapter 10 adding a registration page

--- collection/views.py
+++ collection/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, render_to_response, redirect
 from django.template import RequestContext
 from collection.forms import ThingForm
 from collection.models import Thing
+from django.template.defaultfilters import slugify
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # the rewritten view!
 def index(request):
@@ -18,16 +21,23 @@ def thing_detail(request, slug):
         'thing': thing,
         }, context_instance=RequestContext(request))
 
-# add below your thing_detail view
+@login_required
 def edit_thing(request, slug):
     # grab the object
     thing = Thing.objects.get(slug=slug)
+    # grab the current logged in user and make sure they're the
+    # owner of the thing
+    user=request.user
+    if thing.user != user:
+        raise Http404
+
     # set the form we're using...
     form_class = ThingForm
 
     # if we're coming to this view from a submitted form,
     if request.method == 'POST':
-        # grab the data from the submitted form
+        # grab the data from the submitted form and apply to
+        # the form
         form = form_class(data=request.POST, instance=thing)
         if form.is_valid():
             # save the new data
@@ -44,3 +54,35 @@ def edit_thing(request, slug):
         'form': form,
         }, context_instance=RequestContext(request))
 
+# add below your edit_thing view
+def create_thing(request):
+    # request.user is the logged in user, we're going to assign
+    # it to "user" to make it easy
+    user = request.user
+    form_class = ThingForm
+    # if we're coming from a submitted form, do this
+    if request.method == 'POST':
+        # grab the data from the submitted form and apply to
+        # the form
+        form = form_class(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            description = form.cleaned_data['description']
+            # create the slug from our name
+            slug = slugify(name)
+            # create our object
+            thing = Thing.objects.create(
+                name=name,
+                description=descrition,
+                slug=slug,
+                user=user,
+                )
+        # redirect to our newly created thing
+        return redirect('thing_detail', slug=thing.slug)
+
+    else:
+        form=form_class()
+
+    return render_to_response('things/create_thing.html', {
+        'form':form,
+        }, context_instance=RequestContext(request))
