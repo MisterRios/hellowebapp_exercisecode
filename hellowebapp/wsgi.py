c57ae7966fa8509351fff8e635bb0d3f3945c339
misterrios <misterrios@gmail.com>
2015-01-18 22:54:40 +0100

    added production settings for heroku

--- hellowebapp/wsgi.py
+++ hellowebapp/wsgi.py
@@ -11,5 +11,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings_production")
 application = Cling(get_wsgi_application())
